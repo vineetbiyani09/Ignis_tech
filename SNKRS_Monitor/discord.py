c56327d53c08ee7dae5157f6830e2a594215fccd
Vineet Biyani <67839378+vineetbiyani09@users.noreply.github.com>
2021-05-03 14:15:57 +0530

    Create discord.py

---
+++ SNKRS_Monitor/discord.py
@@ -0,0 +1,36 @@
+from discord_webhook import DiscordWebhook, DiscordEmbed
+
+def hook(product_title, image_url, url, website, category, price, model, stock_status, size_stock, colors) :
+
+    webhook = DiscordWebhook(url = 'YOUR_DISCORD_API_LINK',
+                            username = "SNKRS")
+
+    embed = DiscordEmbed(title = product_title,  url = url, color = '03b2f8')
+
+    embed.set_author(name = 'Nike_SNKRS',
+                    icon_url = 'https://i.pinimg.com/originals/29/c2/e8/29c2e883fb280a99a669fddc80df9088.jpg')
+
+    embed.set_thumbnail(url = image_url)
+
+    embed.set_timestamp()
+
+    embed.add_embed_field(name = 'Stock Status', value = stock_status)
+
+    embed.add_embed_field(name = 'Price', value = price + ' USD')
+
+    embed.add_embed_field(name = 'Category', value = category)
+
+    embed.add_embed_field(name = 'Site', value = website)
+
+    embed.add_embed_field(name = 'Region', value = 'United States')
+
+    embed.add_embed_field(name = 'Model', value = 'Model' + ' ' + model)
+
+    embed.add_embed_field(name='Sizes', value = size_stock)
+
+    embed.add_embed_field(name = 'Colors', value = colors)
+
+    embed.set_footer(text = 'SNKRS Monitor v0.1')
+
+    webhook.add_embed(embed)
+    response = webhook.execute()
